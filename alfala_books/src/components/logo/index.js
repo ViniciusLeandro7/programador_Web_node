40a73a782892083597bc9f4f3001395c2562f3e5
Vinicius <vinicius.lmacedo@senacsp.edu.br>
2025-07-17 22:11:37 -0300

    livraria

--- alfala_books/src/components/logo/index.js
+++ alfala_books/src/components/logo/index.js
@@ -1,12 +1,24 @@
 import logo from '../../img/logo.svg';
-import './style.css' 
-
-function Logo () {
+import styled from 'styled-components';
+ 
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+    align-items: center;
+`
+ 
+const Img = styled.img`
+    height: 60px;
+`
+ 
+function Logo() {
     return (
-        <div className='logo'>
-            <img src={logo} alt='Alfala books logo'></img>
+        <LogoContainer>
+            <Img src={logo} alt='Alfala books logo'></Img>
             <p> <strong>Alfala</strong>Books</p>
-      </div>
+        </LogoContainer>
     )
 }
+ 
 export default Logo;
+ 
