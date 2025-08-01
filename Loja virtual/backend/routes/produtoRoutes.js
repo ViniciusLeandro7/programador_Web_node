6de5e1e687e5ce1fa9fa0d0c98016d6384818b37
Vinicius <vinicius.lmacedo@senacsp.edu.br>
2025-07-31 22:15:22 -0300

    aula

---
+++ Loja virtual/backend/routes/produtoRoutes.js
@@ -0,0 +1,9 @@
+// backend/routes/produtoRoutes.js
+const express = require('express');
+const router = express.Router();
+const produtoController = require('../controllers/produtoController');
+ 
+router.get('/', produtoController.listarProdutos);
+router.get('/:id', produtoController.obterProduto);
+ 
+module.exports = router;
